13462ee93dd31e585fbba311838bb239eb4876a1
jain-tanisha <taniijain26@gmail.com>
2023-08-22 15:44:46 +0530

    all employee UI fixed

--- src/pages/Dashboard/AllEmployee/Employee.js
+++ src/pages/Dashboard/AllEmployee/Employee.js
@@ -62,7 +62,7 @@ const Employee = () => {
               </>
             )}
             <EmployeeHead />
-            <div className="row g-3 row-cols-1 row-cols-sm-1 row-cols-md-1 row-cols-lg-2 row-cols-xl-2 row-cols-xxl-2 row-deck py-1 pb-4 flex-wrap">
+            <div className="row g-3 row-cols-1 row-cols-sm-1 row-cols-md-1 row-cols-lg-2 row-cols-xl-2 row-cols-xxl-2 row-deck py-1 pb-4">
               {employee.map((e, i) => (
                 <EmployeeCard {...e} key={i} />
               ))}
